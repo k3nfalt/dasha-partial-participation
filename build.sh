7306135adb84b30ba57cefdd821211ff949c6cb2
Alexander Tyurin <alexandertiurin@gmail.com>
2022-05-09 11:15:23 +0300

    Added script

---
+++ build.sh
@@ -0,0 +1,7 @@
+pdflatex neurips_2022.tex
+bibtex neurips_2022
+pdflatex neurips_2022.tex
+pdflatex neurips_2022.tex
+mv neurips_2022.pdf supplementary.pdf
+pdflatex '\def\shortpaper{yes} \input{neurips_2022.tex}'
+mv neurips_2022.pdf paper.pdf
